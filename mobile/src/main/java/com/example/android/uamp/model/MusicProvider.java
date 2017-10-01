2719e9edc514f58635ebb67a6fd798a166f33f42
Ákos Muráti <akos@murati.hu>
2017-10-01 14:04:20 +1300

    Fix queuebuilder for ebook

--- mobile/src/main/java/com/example/android/uamp/model/MusicProvider.java
+++ mobile/src/main/java/com/example/android/uamp/model/MusicProvider.java
@@ -470,9 +470,9 @@ public class MusicProvider {
         // can set a hierarchy-aware mediaID. We will need to know the media hierarchy
         // when we get a onPlayFromMusicID call, so we can create the proper queue based
         // on where the music was selected from (by artist, by genre, random, etc)
-        String genre = metadata.getString(MediaMetadataCompat.METADATA_KEY_GENRE);
+        String ebook = metadata.getString(MediaMetadataCompat.METADATA_KEY_ALBUM);
         String hierarchyAwareMediaID = MediaIDHelper.createMediaID(
-                metadata.getDescription().getMediaId(), MEDIA_ID_BY_GENRE, genre);
+                metadata.getDescription().getMediaId(), MEDIA_ID_BY_EBOOK, ebook);
         MediaMetadataCompat copy = new MediaMetadataCompat.Builder(metadata)
                 .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID, hierarchyAwareMediaID)
                 .build();
