2c8fe8082b206cb76da6b7c3bcdabb1903483b74
Ákos Muráti <akos@murati.hu>
2017-10-20 22:52:55 +1300

    Capitalize CategoryKey

--- mobile/src/main/java/com/murati/oszk/audiobook/utils/TextHelper.java
+++ mobile/src/main/java/com/murati/oszk/audiobook/utils/TextHelper.java
@@ -1,9 +1,5 @@
 package com.murati.oszk.audiobook.utils;
 
-/**
- * Created by akosmurati on 20/10/17.
- */
-
 public class TextHelper {
     public static String Capitalize(String input) {
         if (input == null) return "";
