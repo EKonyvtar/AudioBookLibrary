be610144376efa34044e42e4dcb570a8cfb784bf
Akos Murati <akos@murati.hu>
2017-12-04 00:37:16 +1300

    Add about and feedback intents

--- mobile/src/main/java/com/murati/oszk/audiobook/ui/AboutActivity.java
+++ mobile/src/main/java/com/murati/oszk/audiobook/ui/AboutActivity.java
@@ -15,7 +15,11 @@
  */
 package com.murati.oszk.audiobook.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.murati.oszk.audiobook.R;
 
@@ -30,5 +34,16 @@ public class AboutActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         initializeToolbar();
+
+        final Button button = findViewById(R.id.murati);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://murati.hu"));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
