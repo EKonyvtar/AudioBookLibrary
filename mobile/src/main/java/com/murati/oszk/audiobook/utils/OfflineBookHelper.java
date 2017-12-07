e4baf9642b579c1cf9f2477389edd82e43bc1c51
Akos Murati <akos@murati.hu>
2017-12-07 14:58:47 +1300

    Add DownloadService skeleton

---
+++ mobile/src/main/java/com/murati/oszk/audiobook/utils/OfflineBookHelper.java
@@ -0,0 +1,10 @@
+package com.murati.oszk.audiobook.utils;
+
+/**
+ * Created by akos.murati on 5/12/2017.
+ * https://developer.android.com/training/data-storage/files.html
+ */
+
+public class OfflineBookHelper {
+
+}
