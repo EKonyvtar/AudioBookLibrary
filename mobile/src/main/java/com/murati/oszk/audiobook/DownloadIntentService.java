792a498aa50e95b1624cd94b5517058b1ebbe45f
Akos Murati <akos@murati.hu>
2017-12-07 14:58:47 +1300

    Add DownloadService skeleton

---
+++ mobile/src/main/java/com/murati/oszk/audiobook/DownloadIntentService.java
@@ -0,0 +1,36 @@
+package com.murati.oszk.audiobook;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by akos.murati on 7/12/2017.
+ */
+
+public class DownloadIntentService extends IntentService {
+
+    /**
+     * A constructor is required, and must call the super IntentService(String)
+     * constructor with a name for the worker thread.
+     */
+    public DownloadIntentService() {
+        super("DownloadIntentService");
+    }
+
+    /**
+     * The IntentService calls this method from the default worker thread with
+     * the intent that started the service. When this method returns, IntentService
+     * stops the service, as appropriate.
+     */
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        // Normally we would do some work here, like download a file.
+        // For our sample, we just sleep for 5 seconds.
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // Restore interrupt status.
+            Thread.currentThread().interrupt();
+        }
+    }
+}
