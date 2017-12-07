55632d953d09b70df587744f48e6e6f3a6440cc4
Akos Murati <akos@murati.hu>
2017-12-08 00:43:17 +1300

    Move Favorites into helper

--- mobile/src/main/java/com/murati/oszk/audiobook/ui/FullScreenPlayerActivity.java
+++ mobile/src/main/java/com/murati/oszk/audiobook/ui/FullScreenPlayerActivity.java
@@ -19,12 +19,10 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.RemoteException;
 import android.os.SystemClock;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
@@ -36,7 +34,6 @@ import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v4.media.session.PlaybackStateCompat;
 import android.text.format.DateUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
@@ -164,7 +161,7 @@ public class FullScreenPlayerActivity extends ActionBarCastActivity {
             public void onClick(View v) {
                 //TODO: fix for current
                 String ebook = MediaIDHelper.getCategoryValueFromMediaID(MusicProvider.currentEBook);
-                boolean isFavorite = FavoritesHelper.toggleBook(ebook);
+                boolean isFavorite = FavoritesHelper.toggleFavorite(ebook);
 
                 if (isFavorite) mFavoriteButton.setImageResource(R.drawable.ic_star_on);
                 else  mFavoriteButton.setImageResource(R.drawable.ic_star_off);
