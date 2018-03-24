baa78d7e21b936c8949c6d37fa7a6062566d182e
Ákos Muráti <akos@murati.hu>
2018-03-24 22:35:48 +1300

    Set Last Played book

--- mobile/src/main/java/com/murati/oszk/audiobook/utils/PlaybackHelper.java
+++ mobile/src/main/java/com/murati/oszk/audiobook/utils/PlaybackHelper.java
@@ -1,13 +1,9 @@
 package com.murati.oszk.audiobook.utils;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.text.TextUtils;
 import android.util.Log;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Created by akosmurati on 24/03/18.
@@ -16,30 +12,50 @@ import java.io.PrintWriter;
 public class PlaybackHelper {
 
     private static final String TAG = LogHelper.makeLogTag(PlaybackHelper.class);
+    private static final String PLAYBACK_PREFERENCE_FILE = "com.murati.oszk.audiobook.PLAYBACK_PREFERENCE";
+    private static final String PLAYBACK_LAST_POSITION = "PLAYBACK_LAST_POSITION";
+    private static final String PLAYBACK_LAST_MEDIAID = "PLAYBACK_LAST_MEDIAID";
+
 
     private static Context _context = null;
-    private final static String mPersistFilePath = "playback.txt";
 
     private static String _lastMediaId = null;
-    private static int LastPosition = 0;
+    private static int _lastPosition = 0;
 
-    public PlaybackHelper(Context context) {
+    public static void setContext(Context context) {
         _context = context;
+
+        restorePlayBackState();
     }
 
     public static String getLastEBook() {
         if (TextUtils.isEmpty(_lastMediaId))
             return null;
+
         return MediaIDHelper.getParentMediaID(_lastMediaId);
     }
 
     public static void setLastMediaId(String mediaId) {
         _lastMediaId = mediaId;
+
+        persistPlayBackState();
     }
 
-    public void restorePlayBackState() {
-
+    public static void persistPlayBackState() {
+        try {
+            SharedPreferences sharedPref = _context.getSharedPreferences(PLAYBACK_PREFERENCE_FILE,Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString(PLAYBACK_LAST_MEDIAID, _lastMediaId);
+            editor.putInt(PLAYBACK_LAST_POSITION, _lastPosition);
+            editor.commit();
+        } catch (Exception e) {
+            Log.e(TAG, "Playback info cannot be saved. " + e.toString());
+        }
     }
 
-
+    public static void restorePlayBackState() {
+        SharedPreferences sharedPref = _context.getSharedPreferences(PLAYBACK_PREFERENCE_FILE,Context.MODE_PRIVATE);
+        _lastMediaId = sharedPref.getString(PLAYBACK_LAST_MEDIAID, null);
+        _lastPosition = sharedPref.getInt(PLAYBACK_LAST_POSITION, 0);
+    }
 }
