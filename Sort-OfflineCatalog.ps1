cc69e5d9d0c135817cc554a42f3b8927b70329be
Ákos Muráti <akos@murati.hu>
2018-04-27 01:13:26 +1200

    Source everything

--- Sort-OfflineCatalog.ps1
+++ Sort-OfflineCatalog.ps1
@@ -8,4 +8,4 @@ cd $PSScriptRoot
 $catalog = Get-Content -Path $File | ConvertFrom-Json
 $sorted = $catalog.psobject.Copy()
 $sorted.music = $sorted.music | Sort-Object image,trackNumber,source
-Set-Content -Path $File -Value ($sorted | ConvertTo-Json)
+Set-Content -Path $File -Value ($sorted | ConvertTo-Json) -Force -Encoding UTF8
