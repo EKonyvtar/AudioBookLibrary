b32ac9b5a45f15bdb56e42208bdb6bac5285bcde
Ákos Muráti <akos@murati.hu>
2018-04-29 18:03:45 +1200

    Add basic glider load behaviour

---
+++ mobile/src/main/java/com/murati/oszk/audiobook/ui/MyAppGlideModule.java
@@ -0,0 +1,8 @@
+package com.murati.oszk.audiobook.ui;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {}
+
