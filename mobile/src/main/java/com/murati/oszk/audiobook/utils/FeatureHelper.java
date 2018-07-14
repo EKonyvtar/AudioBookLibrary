cda6441ecdba9a9834585a990c25aabd357a0b6f
Ákos Muráti <akos@murati.hu>
2018-07-14 23:33:30 +1200

    Add contentfilter

---
+++ mobile/src/main/java/com/murati/oszk/audiobook/utils/FeatureHelper.java
@@ -0,0 +1,36 @@
+package com.murati.oszk.audiobook.utils;
+
+import android.support.v4.media.MediaBrowserCompat;
+
+import java.util.Locale;
+
+public class FeatureHelper {
+
+    private static String blackList = "^.*(passuth).*$";
+
+    public static boolean isHungarianLocale() {
+        String locale = Locale.getDefault().getLanguage();
+
+        return "hu".equalsIgnoreCase(locale);
+    }
+
+    public static boolean canShowItem(MediaBrowserCompat.MediaItem item) {
+        try {
+            if (!isHungarianLocale()) return true;
+
+            String sample = "";
+            if (item.getDescription().getTitle() != null)
+                sample += item.getDescription().getTitle().toString();
+
+            if (item.getDescription().getSubtitle() != null)
+                sample += " " + item.getDescription().getSubtitle().toString();
+
+            return !sample.toLowerCase().matches(blackList);
+
+        } catch (Exception ex) {
+            //TODO: log
+        }
+
+        return true;
+    }
+}
