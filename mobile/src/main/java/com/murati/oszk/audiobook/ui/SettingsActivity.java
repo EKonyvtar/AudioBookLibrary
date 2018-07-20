56340b97e0c4853f6652f0cb4640a4d8f4697e4b
Ákos Muráti <akos@murati.hu>
2018-07-21 00:59:21 +1200

    Add crashreport to settings

--- mobile/src/main/java/com/murati/oszk/audiobook/ui/SettingsActivity.java
+++ mobile/src/main/java/com/murati/oszk/audiobook/ui/SettingsActivity.java
@@ -16,7 +16,11 @@
 package com.murati.oszk.audiobook.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.crashlytics.android.Crashlytics;
 import com.murati.oszk.audiobook.R;
 
 /**
@@ -28,10 +32,21 @@ public class SettingsActivity extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         //https://firebase.google.com/docs/crashlytics/force-a-crash?authuser=0
-        
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         initializeToolbar();
+
+        Button crashButton = new Button(this);
+        crashButton.setText("Crash!");
+        crashButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Crashlytics.getInstance().crash(); // Force a crash
+            }
+        });
+        addContentView(crashButton,
+            new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
     }
 
 }
