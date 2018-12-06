e430cf4e53a4498e07582b7544c3127c0c60d531
Ákos Muráti <akos@murati.hu>
2018-12-07 00:36:38 +1300

    Add AdHelper

---
+++ mobile/src/main/java/com/murati/oszk/audiobook/utils/AdHelper.java
@@ -0,0 +1,9 @@
+package com.murati.oszk.audiobook.utils;
+
+public class AdHelper {
+
+    public final static int AD_DEFAULT = 0;
+    public final static int AD_EVERYWHERE = 1;
+    public final static int AD_LISTONLY = 2;
+
+}
