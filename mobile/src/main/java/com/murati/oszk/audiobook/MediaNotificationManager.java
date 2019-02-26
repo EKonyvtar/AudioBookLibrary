ef34dcc1883c91f246e586f163f69bc7d200702c
Akos Murati <akos@murati.hu>
2019-02-27 00:36:31 +1300

    Swap out drawables to mipmaps

--- mobile/src/main/java/com/murati/oszk/audiobook/MediaNotificationManager.java
+++ mobile/src/main/java/com/murati/oszk/audiobook/MediaNotificationManager.java
@@ -293,14 +293,14 @@ public class MediaNotificationManager extends BroadcastReceiver {
                 .setMediaSession(mSessionToken))
             .setDeleteIntent(mStopIntent)
             .setColor(mNotificationColor)
-            .setSmallIcon(R.drawable.ic_launcher)
+            .setSmallIcon(R.mipmap.ic_launcher)
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
             .setOnlyAlertOnce(true)
             .setContentIntent(createContentIntent(description))
             .setContentTitle(description.getTitle())
             .setContentText(description.getSubtitle())
             .setLargeIcon(
-                BitmapFactory.decodeResource(mService.getResources(), R.drawable.ic_launcher)
+                BitmapFactory.decodeResource(mService.getResources(), R.mipmap.ic_launcher)
             );
 
         if (mController != null && mController.getExtras() != null) {
