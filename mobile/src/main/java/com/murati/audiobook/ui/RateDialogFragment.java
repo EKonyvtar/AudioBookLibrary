74cd2b432a709036ca134b066b882c7c9beff8af
Akos Murati <akos@murati.hu>
2020-02-02 15:51:02 +0100

    Add basic Rating for hungarian version

---
+++ mobile/src/main/java/com/murati/audiobook/ui/RateDialogFragment.java
@@ -0,0 +1,32 @@
+package com.murati.audiobook.ui;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+import com.murati.audiobook.R;
+import com.murati.audiobook.utils.RateHelper;
+
+public class RateDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.rate_title);
+        builder.setMessage(R.string.rate_message)
+            .setPositiveButton(R.string.rate_ok_button, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    RateHelper.openRating(getContext());
+                }
+            })
+            .setNegativeButton(R.string.rate_no_button, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // User cancelled the dialog
+                }
+            });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
