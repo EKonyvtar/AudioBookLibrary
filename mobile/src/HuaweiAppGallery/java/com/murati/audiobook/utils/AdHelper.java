99e9042b39ead1c772c19c8efecf76e277a8a6a7
Akos Murati <akos@murati.hu>
2020-12-11 03:25:34 +0100

    Remove all GMS dependencies from huawei variant

--- mobile/src/HuaweiAppGallery/java/com/murati/audiobook/utils/AdHelper.java
+++ mobile/src/HuaweiAppGallery/java/com/murati/audiobook/utils/AdHelper.java
@@ -5,9 +5,6 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.huawei.hms.ads.AdListener;
 import com.huawei.hms.ads.AdParam;
