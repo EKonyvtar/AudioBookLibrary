40d8b3973644417ac97c0d7b6cde04212471af40
Akos Murati <akos@murati.hu>
2021-01-15 15:15:55 +0100

    Add Mesekonyvtar ads

--- catalog/Write-OfflineMeseCatalog.ps1
+++ catalog/Write-OfflineMeseCatalog.ps1
@@ -1,8 +1,24 @@
 [CmdletBinding()]
 param (
-	[string]$CatalogFile = './mek_mese_list.txt',
+	[string]$MekFile = './mek_mese_list.txt',
+	[string]$CsvFile = './archive_mese_list.csv',
 	[string]$File = './release/offline_catalog-Mese.json'
 )
 
 Set-Location $PSScriptRoot
-./Write-OfflineMekCatalog.ps1 -CatalogFile $CatalogFile -File $File
+#./Write-OfflineMekCatalog.ps1 -CatalogFile $MekFile -File $File
+
+$catalogContent = Get-Content  -Path $File -Raw
+$catalog = ConvertFrom-Json -InputObject $catalogContent
+$catalog = $catalog.music
+
+Write-Host "----- Merging catalogue ----- "
+$csvContent = Get-Content  -Path $CsvFile -Raw
+$csv = ConvertFrom-Csv -InputObject $csvContent -Delimiter ','
+$catalog += $csv
+
+Write-Host "----- Rewriting catalogue ----- "
+$sortedProperty = ($catalog[0] | Get-Member -Type NoteProperty | Select-Object -Expand Name) | Where-object { $_ -notmatch "site|total" }
+$sorted = New-object psobject -Property @{music = $catalog }
+$sorted.music = $sorted.music | Sort-Object image, trackNumber, source | Select-Object -Property $sortedProperty 
+Set-Content -Path $File -Value ($sorted | ConvertTo-Json) -Force -Encoding UTF8
--- mobile/src/main/java/com/murati/audiobook/utils/FeatureHelper.java
+++ mobile/src/main/java/com/murati/audiobook/utils/FeatureHelper.java
@@ -3,9 +3,12 @@ package com.murati.audiobook.utils;
 import android.support.v4.media.MediaBrowserCompat;
 import android.util.Log;
 
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+
 import java.util.Locale;
 
 public class FeatureHelper {
+    private static final String TAG = LogHelper.makeLogTag(FeatureHelper.class);
 
     private static String blackList = "^.*(passuth).*$";
 
@@ -27,11 +30,20 @@ public class FeatureHelper {
                 sample += " " + item.getDescription().getSubtitle().toString();
 
             return !sample.toLowerCase().matches(blackList);
-
         } catch (Exception ex) {
             //TODO: log
         }
 
         return true;
+    }
+
+    public static boolean canShowDonation(FirebaseRemoteConfig mFirebaseRemoteConfig) {
+        try {
+            String adPosition = mFirebaseRemoteConfig.getString("donation");
+            return (Integer.parseInt(adPosition) == 1);
+        } catch (Exception ex) {
+            Log.e(TAG, ex.getMessage());
+        }
+        return false;
     }
 }
